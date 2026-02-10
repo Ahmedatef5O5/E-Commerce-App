d1e6f72405eefc12470553ef0aedba5d816a4c38
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-10 20:40:50 +0200

    refactor: update CartState to include subtotal and add SubtotalUpdated state

--- lib/cubit/Cart_cubit/cart_state.dart
+++ lib/cubit/Cart_cubit/cart_state.dart
@@ -10,13 +10,20 @@ final class CartLoading extends CartState {}
 
 final class CartLoaded extends CartState {
   final List<AddToCartModel> cartItems;
-  const CartLoaded({required this.cartItems});
+  final double subtotal;
+  const CartLoaded({required this.subtotal, required this.cartItems});
 }
 
 final class QuantityCounterLoaded extends CartState {
   final int value;
   final String productId;
   QuantityCounterLoaded({required this.value, required this.productId});
+}
+
+final class SubtotalUpdated extends CartState {
+  final double subtotal;
+
+  SubtotalUpdated({required this.subtotal});
 }
 
 final class CartFailure extends CartState {
