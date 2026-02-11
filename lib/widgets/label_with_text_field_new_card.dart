441dc65265be9afa90d40cccc56e11efac5ceec4
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-11 18:55:16 +0200

    feat: create reusable LabelWithTextFieldNewCard widget with custom prefix icon support

---
+++ lib/widgets/label_with_text_field_new_card.dart
@@ -0,0 +1,52 @@
+import 'package:ecommerce_app/utilities/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class LabelWithTextFieldNewCard extends StatelessWidget {
+  const LabelWithTextFieldNewCard({
+    super.key,
+    required this.labelTxt,
+    required this.hintTxt,
+    this.prefixIcon,
+    this.controller,
+  });
+  final String labelTxt, hintTxt;
+  final Widget? prefixIcon;
+  final TextEditingController? controller;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Gap(12),
+        Text(
+          labelTxt,
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+            fontWeight: FontWeight.w500,
+            fontSize: 17,
+            color: AppColors.blackColor,
+          ),
+        ),
+        Gap(12),
+        TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            prefixIcon: prefixIcon,
+            hintText: hintTxt,
+            hintStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              fontWeight: FontWeight.w400,
+              fontSize: 17,
+              color: Colors.grey,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
