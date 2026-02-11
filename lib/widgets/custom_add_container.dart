e4f99bee6603d911b4454de1339d91720be8e1d7
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-11 02:01:48 +0200

    feat: create CustomAddContainer widget for checkout addition actions

---
+++ lib/widgets/custom_add_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class CustomAddContainer extends StatelessWidget {
+  const CustomAddContainer({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.blueGrey.shade100, width: 1.2),
+        color: Colors.grey[200],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Icon(CupertinoIcons.add), Gap(10), Text(title)],
+        ),
+      ),
+    );
+  }
+}
