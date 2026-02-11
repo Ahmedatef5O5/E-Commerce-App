d8bf2def7d855f00956a99af37b5d08cebae1d4e
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-11 02:13:53 +0200

    feat: add logic for calculating subtotal, total amount, and product count in CheckoutCubit to using at checkout view

---
+++ lib/cubit/Checkout_cubit/checkout_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:ecommerce_app/models/add_to_cart_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'checkout_state.dart';
+
+class CheckoutCubit extends Cubit<CheckoutState> {
+  CheckoutCubit() : super(CheckoutInitial());
+  final double shiping = 6.00;
+  void getCartItems() {
+    emit(CheckoutLoading());
+    final cartItems = dummyCart;
+    final subtotal = cartItems.fold(
+      0.0,
+      (prev, curr) => prev + (curr.product.price * curr.quantity),
+    );
+    final numOfProducts = cartItems.fold(
+      0,
+      (prev, curr) => prev + curr.quantity,
+    );
+    emit(
+      CheckoutLoaded(
+        numOfProducts: numOfProducts,
+        cartItems: cartItems,
+        totalAmount: subtotal + shiping,
+      ),
+    );
+  }
+}
