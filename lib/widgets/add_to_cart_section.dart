9b48c16ac163aff8eeec3721d41cf76fd99cc722
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-02-13 17:29:19 +0200

    refactor: replace add-to-cart buttons with reusable CustomElevatedButton

--- lib/widgets/add_to_cart_section.dart
+++ lib/widgets/add_to_cart_section.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/cubit/Cart_cubit/cart_cubit.dart';
 import 'package:ecommerce_app/cubit/Product_details_cubit/product_details_cubit.dart';
 import 'package:ecommerce_app/utilities/app_colors.dart';
+import 'package:ecommerce_app/widgets/custom_elevated_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -48,71 +49,46 @@ class AddToCartSection extends StatelessWidget {
                 current is ProductAddingToCart || current is ProductAddedToCart,
             builder: (context, state) {
               if (state is ProductAddingToCart) {
-                return SizedBox(
-                  height: 55,
-                  width: 175,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.greyWithShade,
-                      // backgroundColor: AppColors.primaryColor,
-                      // foregroundColor: AppColors.blackColor,
-                    ),
-                    onPressed: null,
-                    child: Center(
-                      child: CupertinoActivityIndicator(color: Colors.black12),
-                    ),
+                return CustomElevatedButton(
+                  width: 172,
+                  height: 50,
+                  backgroundColor: AppColors.greyWithShade,
+                  onPressed: null,
+                  child: Center(
+                    child: CupertinoActivityIndicator(color: Colors.black12),
                   ),
                 );
               } else if (state is ProductAddedToCart) {
-                return SizedBox(
-                  height: 55,
-                  width: 175,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.greyWithShade,
-                      // backgroundColor: AppColors.primaryColor,
-                      foregroundColor: Colors.black26,
-                    ),
-                    onPressed: null,
-                    child: Center(
-                      child: Text(
-                        'Added to Cart',
-                        style: Theme.of(context).textTheme.titleMedium!
-                            .copyWith(color: Colors.black45),
-                      ),
-                    ),
-                  ),
+                return CustomElevatedButton(
+                  width: 165,
+                  height: 50,
+                  labelTxtbtn: 'Added to Cart',
+                  backgroundColor: AppColors.greyWithShade,
+                  foregroundColor: Colors.black26,
+                  onPressed: null,
                 );
               } else {
-                return SizedBox(
-                  height: 55,
-                  width: 175,
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).primaryColor,
-                      // backgroundColor: AppColors.primaryColor,
-                      foregroundColor: AppColors.whiteColor,
-                    ),
-                    onPressed: () {
-                      if (cubit.selectedSize != null) {
-                        final newItem = cubit.addToCart(productId);
-                        if (newItem != null) {
-                          context.read<CartCubit>().addProductToCart(newItem);
-                        }
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Please select size')),
-                        );
-                      }
-                    },
-                    label: Text(
-                      'Add to Cart',
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: AppColors.whiteColor,
-                      ),
-                    ),
-                    icon: Icon(Icons.shopping_bag_outlined, size: 22),
+                return CustomElevatedButton(
+                  width: 172,
+                  height: 50,
+                  labelTxtbtn: 'Add to Cart',
+                  leadingIcon: Icon(
+                    Icons.shopping_bag_outlined,
+                    color: Colors.white,
+                    size: 22,
                   ),
+                  onPressed: () {
+                    if (cubit.selectedSize != null) {
+                      final newItem = cubit.addToCart(productId);
+                      if (newItem != null) {
+                        context.read<CartCubit>().addProductToCart(newItem);
+                      }
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Please select size')),
+                      );
+                    }
+                  },
                 );
               }
             },
